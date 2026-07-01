2b57fef712fc7d96081dff39bd14cdc67284d038
bartuozdamar <bartuozdamar2@gmail.com>
2026-07-01 22:36:32 +0100

    Delete 05_verify.sql

--- 05_verify.sql
+++
@@ -1,6 +0,0 @@
--- expected: 3819 / 1590 / 10768 / 25754 / 51290
-SELECT 'geography'  AS table_name, count(*) FROM geography
-UNION ALL SELECT 'customers',  count(*) FROM customers
-UNION ALL SELECT 'products',   count(*) FROM products
-UNION ALL SELECT 'orders',     count(*) FROM orders
-UNION ALL SELECT 'order_item', count(*) FROM order_item;
